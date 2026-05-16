5c2be97bc6b355a21a9603f1ce20ef57f12c4dba
FloriMecaj <florimecaj31@gmail.com>
2026-05-16 17:43:12 +0200

    HanaCornerCaffe

--- client/components/CustomCursor.tsx
+++ client/components/CustomCursor.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 
 export default function CustomCursor() {
   const [dot, setDot] = useState({ x: 0, y: 0 });
-  const [ring, setRing] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const onMove = (event: MouseEvent) => {
@@ -15,21 +14,9 @@ export default function CustomCursor() {
     return () => window.removeEventListener("mousemove", onMove);
   }, []);
 
-  useEffect(() => {
-    const timer = window.setInterval(() => {
-      setRing((current) => ({
-        x: current.x + (dot.x - current.x) * 0.34,
-        y: current.y + (dot.y - current.y) * 0.34
-      }));
-    }, 16);
-
-    return () => window.clearInterval(timer);
-  }, [dot]);
-
   return (
     <div className="pointer-events-none fixed inset-0 z-[60] hidden md:block">
       <div className="custom-cursor-dot" style={{ transform: `translate(${dot.x}px, ${dot.y}px)` }} />
-      <div className="custom-cursor-ring" style={{ transform: `translate(${ring.x}px, ${ring.y}px)` }} />
     </div>
   );
 }
